8b453d39150b220ed5f3a426335ae1b28663dbf9
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2024-02-28 17:37:25 +0200

    add home page

--- setting/views.py
+++ setting/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from product.models import Product,Brand,Review
+from django.db.models import Count
 
 # Create your views here.
+def Home(request):
+    brands=Brand.objects.all(). annotate(product_count=Count('product_brand'))
+    sale_products= Product.objects.filter(flag='Sale')[:10]
+    featured_products= Product.objects.filter(flag='Featured')[:6]
+    new_products= Product.objects.filter(flag='New')[:10]
+    reviews= Review.objects.all()[:5]
+
+
+
+    return render (request,'setting/Home.html',{
+        'brands':brands,
+        'sale_products':sale_products,
+        'featured_products':featured_products,
+        'new_products':new_products,
+        'reviews':reviews,
+
+
+
+    })
