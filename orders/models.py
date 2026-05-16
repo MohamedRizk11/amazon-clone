60e0b5b29027a2d6cb706bf08ca738803eaa28aa
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2026-05-16 22:32:33 +0300

    create arders app

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from djago.con
 # Create your models here.
+class Card(models.Model):
+    pass
+
+class Carddetail(models.Model):
+    pass
+
+class order(models.Model):
+    pass
+
+class orderdetail(models.Model):
+    pass
